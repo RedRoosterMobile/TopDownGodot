deda2a9a3f7cbec17af3fe33466ee0ad25dc7432
Mojo Major <info@redroostermobile.com>
2024-10-02 23:50:32 +0200

    fixes

--- scripts/flame_thrower.gd
+++ scripts/flame_thrower.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var burner_scene = preload("res://scenes/burner.tscn")
+@onready var flame_thrower: CPUParticles2D = $FlameThrower
 
 @export var is_active: bool = true
 @export var spawn_interval: float = 0.1  # Spawn every 2 seconds
@@ -9,7 +10,7 @@ var time_passed: float = 0.0
 func _ready() -> void:
 	create_burner()
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# Increment time_passed by delta (the time between frames)
 	time_passed += delta
 	
@@ -17,10 +18,19 @@ func _process(delta: float) -> void:
 	if is_active and time_passed >= spawn_interval:
 		create_burner()
 		time_passed = 0.0  # Reset the timer
-
+	
+	if is_active and not flame_thrower.emitting:
+		print("play ft sound")
+	elif is_active:
+		print("loop ft sound?")
+	else:
+		print("stop ft sound")
+		
+	flame_thrower.emitting = is_active
+	
 func create_burner():
 	var burner_instance: RigidBody2D = burner_scene.instantiate()
 	burner_instance.position = global_position
 	
 	burner_instance.direction = Vector2.RIGHT.rotated(global_rotation)
-	get_tree().root.call_deferred("add_child", burner_instance)
+	get_tree().root.add_child( burner_instance)
