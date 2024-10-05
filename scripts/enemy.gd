2fb9736d3fcefda0769db4c57fe7d0998285d7af
Mojo Major <info@redroostermobile.com>
2024-10-05 16:04:29 +0200

    refactoring

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,7 +5,8 @@ var motion := Vector2()
 var is_dead := false
 var was_hit := false
 var is_burning := false
-var time_till_burnout: float = 3.0
+var burnout_time: float = 3.0
+var time_till_burnout: float = burnout_time
 var speed: float = 5.0
 var rt_node: Node2D
 
@@ -123,116 +124,124 @@ var blood_timer_enabled = true
 var blood_path_start_pos = Vector2.ZERO
 func _physics_process(delta) -> void:
 	if is_dead:
-		if blood_spawn_timer <= 0 and blood_timer_enabled:
-		#if(blood_spawn_timer<=0 and blood_timer_enabled and not is_burning):
-			var blood_path: Node2D = blood_path_scene.instantiate()
-			blood_path.position = global_position
-			if (blood_path_start_pos == Vector2.ZERO):
-				blood_path_start_pos = global_position
-			#blood_path.rotation_degrees = randi_range(1,4)*90
-			blood_path.rotation = blood_path.rotation + get_angle_to(player.global_position)
-			blood_path.scale = Vector2(4, 4)
-			blood_path.modulate.a = randf_range(0.8, 1)
-			blood_path.modulate.h = randf_range(0.5, 0.9)
-			
-			player.draw_me_add(blood_path)
-			blood_spawn_timer = blood_spawn_interval + 0.1
-		else:
-			blood_spawn_timer -= delta
-		move_and_slide()
-		# reduce velocity over time
-		velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
-		return
-	if was_hit:
-		move_and_slide()
-		# reduce velocity over time
-		velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
-		return
-	
-	if enemy_state == Enums.EnemyState.ATTACHED:
-		var distance = player.position.distance_to(position)
-		velocity = velocity.move_toward(position.direction_to(player.position) * distance, 10000 * delta)
-		# remove collision layers?
-		move_and_slide()
-		return
-	
-	#region awareness
-	# set target if not set
-	
-	# find target
+		handle_death(delta)
+	elif was_hit:
+		handle_hit(delta)
+	elif enemy_state == Enums.EnemyState.ATTACHED:
+		handle_attached_state(delta)
+	else:
+		handle_awareness(delta)
+		handle_burning(delta)
+
+func handle_death(delta: float) -> void:
+	if blood_spawn_timer <= 0 and blood_timer_enabled:
+		spawn_blood_path()
+	else:
+		blood_spawn_timer -= delta
+	move_and_slide()
+	velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
+
+func spawn_blood_path() -> void:
+	var blood_path: Node2D = blood_path_scene.instantiate()
+	blood_path.position = global_position
+	if blood_path_start_pos == Vector2.ZERO:
+		blood_path_start_pos = global_position
+	blood_path.rotation += get_angle_to(player.global_position)
+	blood_path.scale = Vector2(4, 4)
+	blood_path.modulate.a = randf_range(0.8, 1)
+	blood_path.modulate.h = randf_range(0.5, 0.9)
+	player.draw_me_add(blood_path)
+	blood_spawn_timer = blood_spawn_interval + 0.1
+
+func handle_hit(delta: float) -> void:
+	move_and_slide()
+	velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
+
+func handle_attached_state(delta: float) -> void:
+	var distance = player.position.distance_to(position)
+	velocity = velocity.move_toward(position.direction_to(player.position) * distance, 10000 * delta)
+	move_and_slide()
+
+func handle_awareness(delta: float) -> void:
 	aquire_target()
-	
 	if target:
-		# TODO: add all circles, done
-		var distance_to_player: float = player.position.distance_to(self.position)
-		
-		var is_in_jump_zone: bool = distance_to_player <= jump_circle
-		var is_in_visual_awareness_zone: bool = (distance_to_player > jump_circle) and (distance_to_player <= visual_awareness_circle)
-		var is_in_noise_awareness_zone: bool = (distance_to_player > visual_awareness_circle) and (distance_to_player <= noise_awareness_circle)
-		
-		if is_in_visual_awareness_zone:
-			enemy_state = Enums.EnemyState.VISUAL
-			navigation_agent_2d.target_position = player.global_position
-			anim_zombie.play("fly")
-			look_at(player.position)
-			# next two lines are the same
-			#motion = (navigation_agent_2d.get_next_path_position()- position).normalized() * speed
-			motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed
-			move_and_collide(motion)
-		elif is_in_jump_zone:
-			if enemy_state != Enums.EnemyState.JUMP:
-				play_growl(11 - 1)
-			enemy_state = Enums.EnemyState.JUMP
-			navigation_agent_2d.target_position = player.global_position
-			anim_zombie.play("fly")
-			look_at(player.position)
-			# next two lines are the same
-			motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed * 2
-			move_and_collide(motion)
-		elif is_in_noise_awareness_zone:
-			enemy_state = Enums.EnemyState.NOISE
-			var angle: float = get_angle_to(player.position)
-			anim_zombie.play("walk")
-			# print(player.get_real_velocity().length_squared() > 400)
-			# turn around first, before walking
-			if abs(angle) >= 0.1:
-				slowly_turn_towards(angle)
-				target = null
-			else:
-				navigation_agent_2d.target_position = player.global_position
-				look_at(player.position)
-				motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed / 2
-				move_and_collide(motion)
-		else:
-			# never happens?
-			enemy_state = Enums.EnemyState.IDLE
-			anim_zombie.play("walk")
-			anim_zombie.pause()
+		handle_target_awareness(delta)
 	else:
 		enemy_state = Enums.EnemyState.IDLE
-		anim_zombie.pause() # aka IDLE
-	if navigation_agent_2d.is_navigation_finished():
-		target = null
-		#return
+		anim_zombie.pause()
 
-	# this overwrites some previous stuff, careful when moving
+func handle_target_awareness(delta: float) -> void:
+	var distance_to_player: float = player.position.distance_to(self.position)
+	var is_in_jump_zone: bool = distance_to_player <= jump_circle
+	var is_in_visual_awareness_zone: bool = (distance_to_player > jump_circle) and (distance_to_player <= visual_awareness_circle)
+	var is_in_noise_awareness_zone: bool = (distance_to_player > visual_awareness_circle) and (distance_to_player <= noise_awareness_circle)
+	
+	if is_in_visual_awareness_zone:
+		handle_visual_awareness()
+	elif is_in_jump_zone:
+		handle_jump_awareness()
+	elif is_in_noise_awareness_zone:
+		handle_noise_awareness()
+	else:
+		enemy_state = Enums.EnemyState.IDLE
+		anim_zombie.play("walk")
+		anim_zombie.pause()
+
+func handle_visual_awareness() -> void:
+	enemy_state = Enums.EnemyState.VISUAL
+	navigation_agent_2d.target_position = player.global_position
+	anim_zombie.play("fly")
+	look_at(player.position)
+	motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed
+	move_and_collide(motion)
+
+func handle_jump_awareness() -> void:
+	if enemy_state != Enums.EnemyState.JUMP:
+		play_growl(11 - 1)
+	enemy_state = Enums.EnemyState.JUMP
+	navigation_agent_2d.target_position = player.global_position
+	anim_zombie.play("fly")
+	look_at(player.position)
+	motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed * 2
+	move_and_collide(motion)
+
+func handle_noise_awareness() -> void:
+	enemy_state = Enums.EnemyState.NOISE
+	var angle: float = get_angle_to(player.position)
+	anim_zombie.play("walk")
+	if abs(angle) >= 0.1:
+		slowly_turn_towards(angle)
+		target = null
+	else:
+		navigation_agent_2d.target_position = player.global_position
+		look_at(player.position)
+		motion = position.direction_to(navigation_agent_2d.get_next_path_position()) * speed / 2
+		move_and_collide(motion)
+
+func handle_burning(delta: float) -> void:
 	if is_burning:
 		if time_till_burnout <= 0:
-			print("all burnt out")
-			is_dead = true
-			health = 0.0
-			enemy_state = Enums.EnemyState.DEAD
-			timer.stop()
-			anim_zombie.pause()
-			anim_zombie.play("die")
-			fire.visible = false
-			fire.emitting = false
+			handle_burnout()
 		else:
-			# slowly make him darker
-			anim_zombie.modulate.r -= 0.003
-			anim_zombie.modulate.g -= 0.003
-			anim_zombie.modulate.b -= 0.003
 			time_till_burnout -= delta
+			
+			var vv = time_till_burnout/burnout_time
+			# shitty randomness
+			var tt = clamp(vv+randf_range(0.1,0.5),0.0,1.0)
+			
+			anim_zombie.material.set_shader_parameter("tint", tt)
+			# for shader we need to to it ourselves
+
+func handle_burnout() -> void:
+	print("all burnt out")
+	is_dead = true
+	health = 0.0
+	enemy_state = Enums.EnemyState.DEAD
+	timer.stop()
+	anim_zombie.pause()
+	anim_zombie.play("die")
+	fire.visible = false
+	fire.emitting = false
 
 func slowly_turn_towards(target_angle, turn_speed = 0.02) -> void:
 	rotation = lerp_angle(rotation, rotation + target_angle, turn_speed)
@@ -338,7 +347,7 @@ func _on_area_2d_body_entered(body) -> void:
 		# start the fire!
 	elif body.is_in_group("shrapnel"):
 		Messenger.screenshake.emit(1)
-		var shrapnel_force:float = randf_range(0.8, 1)
+		var shrapnel_force: float = randf_range(0.8, 1)
 		health -= shrapnel_force
 		if health <= 0.0:
 			is_dead = true
@@ -365,7 +374,7 @@ func _on_area_2d_body_entered(body) -> void:
 		anim_impact.play()
 		# Apply impact force
 		var impact_force = randf_range(150 * 2, 190 * 2)
-		velocity += impact_direction * impact_force*shrapnel_force
+		velocity += impact_direction * impact_force * shrapnel_force
 		print("enemy hit by a shrapnel")
 
 func _on_timer_timeout() -> void:
@@ -383,6 +392,12 @@ func _on_anim_zombie_animation_finished() -> void:
 	blood_timer_enabled = false
 	velocity = Vector2.ZERO
 	anim_zombie.pause()
+	
+	# transfer tint from shader to enemy for painting
+	var tint = anim_zombie.material.get_shader_parameter("tint")
+	anim_zombie.modulate.r = tint
+	anim_zombie.modulate.g = tint
+	anim_zombie.modulate.b = tint
 	anim_zombie.material = null
 	if is_burning:
 		# don't draw fire
